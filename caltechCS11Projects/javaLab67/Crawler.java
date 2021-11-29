a6c29e42b1e09eda4f2033dab449b17981157b81
Nicholas Hung <nhung93@outlook.com>
2021-11-28 20:48:21 -0500

    reviewing old java stuff lol and hopefully improving stuff

--- caltechCS11Projects/javaLab67/Crawler.java
+++ caltechCS11Projects/javaLab67/Crawler.java
@@ -37,11 +37,12 @@ public class Crawler{
 	 * out of the function and be caught by the try/catch outside it.
 	 * also, before processing each URL, check if currentDepth < maxDepth
 	**/
-	public void processURL(String a) throws Exception{
-		
+	public void processURL(String urlStr) throws Exception{
 		//host should be in the format: "http://.....com" with possible path.
 		//URL object should fix up the string 
-		URL url = new URL(a); //URLDepthPair.getHost(a);
+        System.out.println("checking: " + urlStr);
+        
+		URL url = new URL(urlStr); //URLDepthPair.getHost(a);
 		String host = url.toString();
 		
 		//create initial URLDepthPair
@@ -51,122 +52,119 @@ public class Crawler{
 		visitedSites.add(initialPair);
 		
 		//then explore the initial URL
-			//attempt to connect (port by default is 80)
-			Socket sock = new Socket(url.getHost(), 80);
-			sock.setSoTimeout(3000);
-			
-			//if successful...
-			//set up outputstream to send info to host server
-			OutputStream os = sock.getOutputStream();
-			
-			//then set up a writer and hook up the outputstream to it
-			//this sends a request to the host server
-			PrintWriter writer = new PrintWriter(os, true);
-			writer.println("GET " + URLDepthPair.getDocPath(a) + " HTTP/1.1");
-			writer.println("Host: " + url.getHost());
-			writer.println("Connection: close");
-			writer.println();
-			
-			//request should be sent. now we can read the web page.
-			InputStream is = sock.getInputStream();
-			InputStreamReader isr = new InputStreamReader(is);
-			BufferedReader br = new BufferedReader(isr);
-			
-			//read the web page and find URLs
-			while (true){
-				String line = br.readLine();
-				//System.out.println(line);
-				if(line == null){
-					//by now, all possible URLs for one page
-					//should've been looked at.
-					//therefore, one depth has been looked at.
-					//increment the currentDepth by one.
-					currentDepth++;
-					break;
-				}else if(!URLDepthPair.checkLine(line)){
-					//if we come across a non-valid URL,
-					//skip it and continue
-					continue;
-				}else{
-					//find a match for a valid URL
-					//make URLDepthPair, put in linked list
-					if(currentDepth < maxDepth){
-						URLDepthPair.parseURL(line, currentDepth, sitesToVisit);
-					}else{
-						break;
-					}
-				}
-			}
-			//close socket when done
-			sock.close();
-		}
+        //attempt to connect (port by default is 80)
+        Socket sock = new Socket(url.getHost(), 80);
+        sock.setSoTimeout(3000);
+        
+        //if successful...
+        //set up outputstream to send info to host server
+        OutputStream os = sock.getOutputStream();
+        
+        //then set up a writer and hook up the outputstream to it
+        //this sends a request to the host server
+        PrintWriter writer = new PrintWriter(os, true);
+        writer.println("GET " + URLDepthPair.getDocPath(urlStr) + " HTTP/1.1");
+        writer.println("Host: " + url.getHost());
+        writer.println("Connection: close");
+        writer.println();
+        
+        //request should be sent. now we can read the web page.
+        InputStream is = sock.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        
+        //read the web page and find URLs
+        while(true){
+            String line = br.readLine();
+            System.out.println(line);
+            if(line == null){
+                //by now, all possible URLs for one page
+                //should've been looked at.
+                //therefore, one depth has been looked at.
+                //increment the currentDepth by one.
+                currentDepth++;
+                break;
+            }else if(!URLDepthPair.checkLine(line)){
+                //if we come across a non-valid URL,
+                //skip it and continue
+                continue;
+            }else{
+                //find a match for a valid URL
+                //make URLDepthPair, put in linked list
+                if(currentDepth < maxDepth){
+                    URLDepthPair.parseURL(line, currentDepth, sitesToVisit);
+                }else{
+                    break;
+                }
+            }
+        }
+        //close socket when done
+        sock.close();
+    }
 	
 
 	public static void main(String[] args) throws Exception{
-		
 		/*
 	
-	//let's assume user will input correctly for now
-	Scanner scan = new Scanner(System.in);
-	
-	System.out.println("please enter a url.");
-	String webHost = scan.nextLine();
-	
-	System.out.println("please enter a non-negative integer that will represent the search depth.");
-	int searchDepth = scan.nextInt();
-	
-	scan.close();
-	//int webPort = 80;
+        //let's assume user will input correctly for now
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("please enter a url.");
+        String webHost = scan.nextLine();
+        
+        System.out.println("please enter a non-negative integer that will represent the search depth.");
+        int searchDepth = scan.nextInt();
+        
+        scan.close();
+        //int webPort = 80;
 	
 		*/
-	
-	//set up new crawler
-	Crawler crawler1 = new Crawler(2);
-	
-	//process a url for crawler1
-	try{
-		
-	//http://users.cms.caltech.edu/~donnie/testcrawl/
-	//www.cms.caltech.edu/people
+    
+        //set up new crawler
+        Crawler crawler1 = new Crawler(2);
+        
+        //process a url for crawler1
+        try{
+            //http://users.cms.caltech.edu/~donnie/testcrawl/
+            //www.cms.caltech.edu/people
 
-	//pass in a URL you want to crawl
-	crawler1.processURL("http://syncopika.tumblr.com");
-	
-	//checked linked list and loop
-	LinkedList<URLDepthPair> sitesToCheck = crawler1.getSites();
-	LinkedList<URLDepthPair> sitesChecked = new LinkedList<URLDepthPair>();;
-	
-	//while(a.size() > 0)
-	while(sitesToCheck.size() > 0){
-		String a = sitesToCheck.getFirst().getURL();
-		crawler1.processURL(a);
-		sitesChecked.add(sitesToCheck.getFirst());
-		sitesToCheck.removeFirst();
-		System.out.println(sitesChecked);
-	}
-		
-	for(int i = 0; i < sitesChecked.size(); i++){
-		System.out.println(sitesChecked.get(i));
-	}
-	
-	}catch(IOException e){
-		System.out.println("IO error hi");
-	}
+            //pass in a URL you want to crawl
+            crawler1.processURL("http://syncopika.tumblr.com");
+            
+            //checked linked list and loop
+            LinkedList<URLDepthPair> sitesToCheck = crawler1.getSites();
+            LinkedList<URLDepthPair> sitesChecked = new LinkedList<URLDepthPair>();;
+            
+            while(sitesToCheck.size() > 0){
+                String site = sitesToCheck.getFirst().getURL();
+                crawler1.processURL(site);
+                sitesChecked.add(sitesToCheck.getFirst());
+                sitesToCheck.removeFirst();
+                System.out.println(sitesChecked);
+            }
+                
+            for(int i = 0; i < sitesChecked.size(); i++){
+                System.out.println(sitesChecked.get(i));
+            }
+        
+        }catch(IOException e){
+            System.out.println("IO error hi");
+        }
 
-	//this is fine if you are using command-line, but I don't really want
-	//to so using scanner to read in user input through the console may be
-	// a good substitute
-	/*
-	if(args.length != 2){
-		System.err.println("Usage: java <host name> <port number>");
-		return;
-	}
-		
-	String webHost = args[0];
-	int webPort = 80;//Integer.parseInt(args[1]);
-	int searchDepth = Integer.parseInt(args[1]);;
-	
-	*/
+        //this is fine if you are using command-line, but I don't really want
+        //to so using scanner to read in user input through the console may be
+        // a good substitute
+        /*
+        if(args.length != 2){
+            System.err.println("Usage: java <host name> <port number>");
+            return;
+        }
+            
+        String webHost = args[0];
+        int webPort = 80;//Integer.parseInt(args[1]);
+        int searchDepth = Integer.parseInt(args[1]);;
+        
+        */
 
 	}
 }
